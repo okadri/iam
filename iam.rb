ba7432fc3c610079a680f355d975f89f884f30db
Obada Kadri <okadri@ucdavis.edu>
2013-09-05 11:03:23 -0700

    Moved fetching an individual to a separate method

--- iam.rb
+++ iam.rb
@@ -4,13 +4,69 @@ require 'yaml'
 load 'UcdLookups.rb'
 IAM_SETTINGS_FILE = "config/iam.yml"
 
+### Import the IAM site and key from the yaml file
 if File.file?(IAM_SETTINGS_FILE)
   $IAM_SETTINGS = YAML.load_file(IAM_SETTINGS_FILE)
-  site = $IAM_SETTINGS['HOST']
-  key = $IAM_SETTINGS['KEY']
+  @site = $IAM_SETTINGS['HOST']
+  @key = $IAM_SETTINGS['KEY']
 else
   puts "You need to set up config/iam.yml before running this application."
   exit
+end
+
+### Method to get individual info
+def fetch_by_iamId(id)
+  ## First, fetch the person
+  url = "#{@site}iam/people/search/?iamId=#{id}&key=#{@key}&v=1.0"
+  # Fetch URL
+  resp = Net::HTTP.get_response(URI.parse(url))
+  # Parse results
+  buffer = resp.body
+  result = JSON.parse(buffer)
+
+  first = result["responseData"]["results"][0]["dFirstName"]
+  middle = result["responseData"]["results"][0]["oMiddleName"]
+  last = result["responseData"]["results"][0]["dLastName"]
+  isEmployee = result["responseData"]["results"][0]["isEmployee"]
+  isFaculty = result["responseData"]["results"][0]["isFaculty"]
+  isStudent = result["responseData"]["results"][0]["isStudent"]
+  isStaff = result["responseData"]["results"][0]["isStaff"]
+
+  ## Second, fetch the contact info
+  url = "#{@site}iam/people/contactinfo/#{id}?key=#{@key}&v=1.0"
+  # Fetch URL
+  resp = Net::HTTP.get_response(URI.parse(url))
+  # Parse results
+  buffer = resp.body
+  result = JSON.parse(buffer)
+
+  email = result["responseData"]["results"][0]["email"]
+  phone = result["responseData"]["results"][0]["workPhone"]
+  address = result["responseData"]["results"][0]["postalAddress"]
+
+  ## Third, fetch the kerberos userid
+  url = "#{@site}iam/people/prikerbacct/#{id}?key=#{@key}&v=1.0"
+  # Fetch URL
+  resp = Net::HTTP.get_response(URI.parse(url))
+  # Parse results
+  buffer = resp.body
+  result = JSON.parse(buffer)
+
+  loginid = result["responseData"]["results"][0]["userId"]
+
+  ## Forth, fetch the association
+  url = "#{@site}iam/associations/pps/search?iamId=#{id}&key=#{@key}&v=1.0"
+  # Fetch URL
+  resp = Net::HTTP.get_response(URI.parse(url))
+  # Parse results
+  buffer = resp.body
+  result = JSON.parse(buffer)
+
+  dept = result["responseData"]["results"][0]["deptOfficialName"]
+  title = result["responseData"]["results"][0]["titleOfficialName"]
+
+  ## Display the results (Or insert them in database)
+  puts "#{id}: #{loginid} -- #{first} #{last} <#{email}> works for #{dept} as a #{title}"
 end
 
 
@@ -21,10 +77,11 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 total = 0
 timestamp_start = Time.now
 
+### In case no arguments are provided, we fetch for all people in UcdLookups departments
 for d in UcdLookups::DEPT_CODES.keys()
 
   ## Fetch department members
-  url = "#{site}iam/associations/pps/search?deptCode=#{d}&key=#{key}&v=1.0"
+  url = "#{@site}iam/associations/pps/search?deptCode=#{d}&key=#{@key}&v=1.0"
 
   # Fetch URL
   resp = Net::HTTP.get_response(URI.parse(url))
@@ -37,63 +94,11 @@ for d in UcdLookups::DEPT_CODES.keys()
 
   # loop over members
   result["responseData"]["results"].each do |p|
-    ## First, fetch the person
-    url = "#{site}iam/people/search/?iamId=#{p['iamId']}&key=#{key}&v=1.0"
-    # Fetch URL
-    resp = Net::HTTP.get_response(URI.parse(url))
-    # Parse results
-    buffer = resp.body
-    result = JSON.parse(buffer)
-    
-    first = result["responseData"]["results"][0]["dFirstName"]
-    middle = result["responseData"]["results"][0]["oMiddleName"]
-    last = result["responseData"]["results"][0]["dLastName"]
-    isEmployee = result["responseData"]["results"][0]["isEmployee"]
-    isFaculty = result["responseData"]["results"][0]["isFaculty"]
-    isStudent = result["responseData"]["results"][0]["isStudent"]
-    isStaff = result["responseData"]["results"][0]["isStaff"]
-
-    ## Second, fetch the contact info
-    url = "#{site}iam/people/contactinfo/#{p['iamId']}?key=#{key}&v=1.0"
-    # Fetch URL
-    resp = Net::HTTP.get_response(URI.parse(url))
-    # Parse results
-    buffer = resp.body
-    result = JSON.parse(buffer)
-    
-    email = result["responseData"]["results"][0]["email"]
-    phone = result["responseData"]["results"][0]["workPhone"]
-    address = result["responseData"]["results"][0]["postalAddress"]
-
-    ## Third, fetch the kerberos userid
-    url = "#{site}iam/people/prikerbacct/#{p['iamId']}?key=#{key}&v=1.0"
-    # Fetch URL
-    resp = Net::HTTP.get_response(URI.parse(url))
-    # Parse results
-    buffer = resp.body
-    result = JSON.parse(buffer)
-    
-    userId = result["responseData"]["results"][0]["userId"]
-
-    ## Forth, fetch the association
-    url = "#{site}iam/associations/pps/search?iamId=#{p['iamId']}&key=#{key}&v=1.0"
-    # Fetch URL
-    resp = Net::HTTP.get_response(URI.parse(url))
-    # Parse results
-    buffer = resp.body
-    result = JSON.parse(buffer)
-
-    dept = result["responseData"]["results"][0]["deptOfficialName"]
-    title = result["responseData"]["results"][0]["titleOfficialName"]
-
-    ## Display the results (Or insert them in database)
-    puts "#{p['iamId']}: #{userId} -- #{first} #{last} <#{email}> works for #{dept} as a #{title}"
+    fetch_by_iamId(p['iamId'])
   end
 
 end
 
-
-puts "Total = #{total}"
 timestamp_finish = Time.now
 
-puts "Finished IAM import. Time elapsed: " + Time.at(timestamp_finish - timestamp_start).gmtime.strftime('%R:%S')
+puts "Finished importing a total of #{total}. Time elapsed: " + Time.at(timestamp_finish - timestamp_start).gmtime.strftime('%R:%S')
